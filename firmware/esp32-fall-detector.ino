f533fcddf06601fd60d6f50c3fe3c36541f135a3
bturbil <114877436+bturbil@users.noreply.github.com>
2026-06-20 23:06:12 +0300

    Hide UUIDs for service and characteristic

--- firmware/esp32-fall-detector.ino
+++ firmware/esp32-fall-detector.ino
@@ -7,8 +7,8 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID        "hidden"
+#define CHARACTERISTIC_UUID "hidden"
 
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
